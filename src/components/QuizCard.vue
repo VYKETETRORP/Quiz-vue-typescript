60045b2243fb2a28c1e55abf6daba8b50aad52f9
VYKETETRORP <lesleygs56@gamil.com>
2024-08-14 10:57:24 +0700

    first commit

---
+++ src/components/QuizCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    quiz
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'QuizCard',
+};
+</script>
+
+<style scoped>
+.wrapper {
+  height: 300px;
+  width: 400px;
+  background-color: white;
+  border-radius: 10px;
+}
+</style>
